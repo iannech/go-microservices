31b99afc181b7aed6e479a4dac3474a07d2343a5
iannech <iaoniango94@gmail.com>
2023-02-17 09:17:15 +0300

    handling timeouts and graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"go-microservices/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -16,5 +19,31 @@ func main() {
 	sm.Handle("/", helloHandler)
 	sm.Handle("/goodbye", goodbyeHandler)
 
-	http.ListenAndServe(":8080", sm)
+	// handling timeouts and graceful shutdown
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: sm,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// to prevent blocking
+	go func ()  {
+		err := server.ListenAndServe()
+		if err != nil{
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	// to shutdown gracefully
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30 * time.Second)
+	server.Shutdown(timeoutContext)
 }
